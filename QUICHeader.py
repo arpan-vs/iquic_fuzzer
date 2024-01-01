d4438d0122e9c4aa2816f0ca672cf6e5dda3bf8c
Nirajkumar Italiya <69964346+NirajItaliya@users.noreply.github.com>
2024-01-01 21:10:20 +0530

    Revert "receive initial packet"
    This reverts commit f81eedec2ef0285d04567abdf86c807e2ce481bc.

--- QUICHeader.py
+++ QUICHeader.py
@@ -1,9 +1,34 @@
-
-
-
 from scapy.fields import *
 from scapy.packet import Packet
+
+
 from utils.string_to_ascii import string_to_ascii
+from utils.packet_to_hex import extract_from_packet, extract_from_packet_as_bytestring
+from scapy.layers.tls.basefields import _tls_version_options
+
+from scapy.layers.tls.handshake import TLSClientHello
+import random
+from utils.SessionInstance import SessionInstance
+
+
+from scapy.layers.tls.extensions import (
+    TLS_Ext_SupportedGroups,
+    TLS_Ext_SupportedVersion_CH,
+    TLS_Ext_SignatureAlgorithms,
+    TLS_Ext_ServerName,
+    ServerName,
+    TLS_Ext_PSKKeyExchangeModes,
+    TLS_Ext_ALPN,
+    ProtocolName,
+    TLS_Ext_Unknown,
+    _tls_ext
+)
+from scapy.layers.tls.keyexchange_tls13 import (
+    TLS_Ext_KeyShare_CH,
+    KeyShareEntry,
+    TLS_Ext_KeyShare_HRR,
+    TLS_Ext_PreSharedKey_CH
+)
 
 class QUICHeader(Packet):
     """
@@ -20,9 +45,54 @@ class QUICHeader(Packet):
         StrFixedLenField("SCID",string_to_ascii("2d35022d62b561f2"),8),
         ByteField("Token_length", 0),
         StrFixedLenField("Length",bytes.fromhex("4496"),2),
+        # StrFixedLenField("Length",(1174).to_bytes(2,byteorder="big"),2),
+        # StrFixedLenField("Length",string_to_ascii("6bafa3cda6256d3c"),2),
         LEShortField("Packet_Number",0),
     ]
 
+class CryptoFrame(Packet) :
+    """
+    The Crypto Frame 
+    """
+    name = "QUIC"
+    fields_desc =[
+        XByteField("Frame_Type", 0x06),
+        ByteField("Offset", 0),
+        StrFixedLenField("Length",bytes.fromhex("4179"),2),
+    ]
+
+parameter_named = { 0x01 : "max_idle_timeout" ,
+                    0x04 : "initial_max_data",
+                    0x05 : "initial_max_stream_data_bidi_local",
+                    0x06 : "initial_max_stream_data_bidi_remote",
+                    0x07 : "initial_max_stream_data_uni",
+                    0x08 : "initial_max_stream_bidi",
+                    0x09 : "initial_max_strem_uni",
+                    0x0a : "ack_delay_exponet",
+                    0x0b : "max_ack_delay",
+                    0x0e : "active_connection_id_limit",
+                    0x0f : "initial_source_connection_id"
+                   }
+
+class Parameter(Packet):
+    name = "Parameter"
+    fields_desc = [
+                # ShortEnumField("Type", None, parameter_named),
+                #    FieldLenField("len", None, length_of="value"),
+                   XByteField("Type",None),
+                   ByteField("len",None),
+                   XStrLenField("value", None,
+                                length_from = lambda pkt: pkt.len) ]
+    
+    
+class QUIC_Ext_Transport_parameters(TLS_Ext_Unknown) :
+     name = "QUIC Extension - Transport parameters"
+     fields_desc = [
+                   ShortEnumField("type", None, _tls_ext),
+                   ShortField("len", None),
+                #    LEShortField("len",None),
+                   PacketListField("Parameters", [], Parameter,
+                                   length_from=lambda pkt: pkt.len)] 
 
 
 
@@ -30,8 +100,43 @@ class QUICHeader(Packet):
 
 
 
+def TLSObject() :
+    ext2 = []
+    ext2 += TLS_Ext_KeyShare_CH(client_shares=[KeyShareEntry(group = "secp256r1"),KeyShareEntry(group = "x25519") ,KeyShareEntry(group = "x448") ],len= 167,client_shares_len=165 )
+    ext2 += TLS_Ext_SupportedVersion_CH(versions=["TLS 1.3"],len = 3,versionslen = 2)
+    ext2 += TLS_Ext_SignatureAlgorithms(sig_algs=[0x0804, 0x0403,0x0401,0x0201,0x0807,0x0808],len = 14 ,sig_algs_len= 12)
+    ext2 += TLS_Ext_SupportedGroups(groups = ["secp256r1", "x25519", "x448"] , len = 8, groupslen = 6)
+    ext2 += TLS_Ext_PSKKeyExchangeModes(kxmodes= 1 ,len= 2,kxmodeslen= 1)
 
+    ext2 += TLS_Ext_ServerName(servernames=[ServerName(servername= "localhost")],len = 14,servernameslen= 12)
+    
+    ext2 += TLS_Ext_ALPN(protocols = [ProtocolName(len = 2 , protocol = "h3") , ProtocolName(len = 5 , protocol = "h3-32" ),ProtocolName(len = 5 , protocol = "h3-31"),ProtocolName(len = 5 , protocol = "h3-30"),ProtocolName(len = 5 , protocol = "h3-29")], len = 29, protocolslen = 27)
 
-
-
-
+    ext2 += QUIC_Ext_Transport_parameters(type = 0x39,Parameters=[
+                                                    Parameter(Type= 0x01,len= 4 , value = bytes.fromhex("8000ea60")),
+                                                    Parameter(Type= 0x04,len= 4 , value = bytes.fromhex("80100000")),
+                                                    Parameter(Type= 0x05,len= 4 , value = bytes.fromhex("80100000")),
+                                                    Parameter(Type= 0x06,len= 4 , value = bytes.fromhex("80100000")),          
+                                                    Parameter(Type= 0x07,len= 4 , value = bytes.fromhex("80100000")),
+                                                    Parameter(Type= 0x08,len= 2 , value = bytes.fromhex("4080")),
+                                                    Parameter(Type= 0x09,len= 2 , value = bytes.fromhex("4080")),
+                                                    Parameter(Type= 0x0a,len= 1 , value = bytes.fromhex("03")),
+                                                    Parameter(Type= 0x0b,len= 1 , value = bytes.fromhex("19")),
+                                                    Parameter(Type= 0x0e,len= 1, value = bytes.fromhex("08")),
+                                                    Parameter(Type= 0x0f,len= 8 , value = string_to_ascii(SessionInstance.get_instance().initial_source_connection_id)),
+                                                    ],len = 57)
+  
+    
+    return TLSClientHello(
+            msglen = 373,
+            version = 0x0303,
+            sidlen=0,
+            sid = None,
+            random_bytes=  "00" * 32  ,#bytes.fromhex(random.getrandbits(32)),
+            cipherslen= 6,
+            ciphers=[0x1302, 0x1301, 0x1303],
+            complen = 1,
+            comp = [0],
+            extlen = 326,
+            ext= ext2
+            ) 
--- utils/dhke.py
+++ utils/dhke.py
@@ -8,13 +8,11 @@ from utils.SessionInstance import SessionInstance
 from typing import Callable, Optional, Tuple
 from aioquic.quic.crypto import hkdf_extract,hkdf_expand_label,cipher_suite_hash , CipherSuite, AEAD, CryptoError, HeaderProtection
 import binascii
-
 INITIAL_CIPHER_SUITE = CipherSuite.AES_128_GCM_SHA256
 INITIAL_SALT_DRAFT_29 = binascii.unhexlify("afbfec289993d24c9e9786f19c6111e04390a899")
 INITIAL_SALT_VERSION_1 = binascii.unhexlify("38762cf7f55934b34d179ae6a4c80cadccbb7f0a")
 
-
-class secret_all:
+class dhke:
 
     # initial_secret = HKDF-Extract(initial_salt, cid)        
     @staticmethod
@@ -32,7 +30,7 @@ class secret_all:
     def client_initial_secret() :
         client_initial_secret = hkdf_expand_label(
             algorithm = hashes.SHA256(),
-            secret = secret_all.initial_secret(string_to_ascii(SessionInstance.get_instance().initial_destination_connection_id)),
+            secret = dhke.initial_secret(string_to_ascii(SessionInstance.get_instance().initial_destination_connection_id)),
             label=b"client in",
             hash_value=b"",
             length = 32,
@@ -42,20 +40,10 @@ class secret_all:
     # server_initial_secret  = HKDF-Expand-Label(initial_secret, "server in", "", 32)
     def server_initial_secret() :
         server_initial_secret = hkdf_expand_label(
-            algorithm = hashes.SHA3_256(),
-            secret = secret_all.initial_secret(string_to_ascii(SessionInstance.get_instance().initial_destination_connection_id)),
+            algorithm = hashes.SHA256(),
+            secret = dhke.initial_secret(string_to_ascii(SessionInstance.get_instance().initial_destination_connection_id)),
             label=b"server in",
             hash_value=b"",
             length = 32,
             )
         return server_initial_secret 
-    
-    def nth_secret(Secret) :
-        nth_secret = hkdf_expand_label(
-            algorithm = hashes.SHA256(),
-            secret = Secret,
-            label=b"quic ku",
-            hash_value=b"",
-            length = 32,
-            )
-        return nth_secret 
